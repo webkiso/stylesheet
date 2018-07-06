336be6b49f6f1d879493df9faeaeb3ec85e9b58c
webkiso <40767048+webkiso@users.noreply.github.com>
2018-07-07 01:58:04 +0900

    Update sakamichi.js

--- sakamichi.js
+++ sakamichi.js
@@ -12362,7 +12362,7 @@ phina.namespace(function() {
       defaults: {
         score: 16,
 
-        message: 'this is phina.js project.',
+        message: '12号館前の坂。緩やかに見えて、上りのチャリは地味にきつい。',
         hashtags: 'phina_js,game,javascript',
         url: phina.global.location && phina.global.location.href,
 
