585d0b7d0737c041077914ac557374a2f0665f31
webkiso <40767048+webkiso@users.noreply.github.com>
2018-07-07 02:08:53 +0900

    Update sakamichi.js

--- sakamichi.js
+++ sakamichi.js
@@ -12299,7 +12299,7 @@ phina.namespace(function() {
               text: message,
               fill: params.fontColor,
               stroke: null,
-              fontSize: 16,
+              fontSize: 14,
             },
             x: this.gridX.center(),
             y: this.gridY.span(9),
